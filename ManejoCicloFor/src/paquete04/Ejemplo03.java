cd06d6e845c3c352f5356cf334e250b4b9d68a48
Andrea <criolloandrea20@hotmail.com>
2024-12-05 12:09:32 -0500

    Ejemplo 03

---
+++ ManejoCicloFor/src/paquete04/Ejemplo03.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete04;
+
+import paquete01.*;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo03 {
+    public static void main(String[] args) {
+               
+        for (int i = 1; i <= 5; i++) {
+            for (int contador = 1; contador <= i; contador++) {
+                
+                System.out.printf("%s", "*");
+            }
+            System.out.println();
+        }
+        
+        for (int i = 5; i >= 1; i--) {
+            for (int contador = 1; contador <= i; contador++) {
+                
+                System.out.printf("%s", "*");
+            }
+            System.out.println();
+        
+    }
+}
+}
